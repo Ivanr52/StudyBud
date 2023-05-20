fe5c317a3f3abe2164182a9f7684b75a1c761f0b
Ivan Rojas <ivan50303@gmail.com>
2023-05-20 10:28:58 -0400

    Finished search, user login, started flash messages

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,4 +1,8 @@
+import email
 from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.db.models import Q
+from django.contrib.auth.models import User
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -10,12 +14,33 @@ from .forms import RoomForm
 #     {'id': 3, 'name': 'Frontend developers'},
 # ] 
 
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        #Check if the user exists. If not, throw error message
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist')
+
+    context = {}
+    return render(request, 'base/login_register.html', context)
+
 def home(request):
-    rooms = Room.objects.all()
+    q = request.GET.get('q') if request.GET.get('q') != None else '' #If q is not None, q=parameter. Else, q is equal to an empty string
+    #Can search by 3 different values: topic name, host name, and description
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) | #search parameter must match/contain either Q.
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        ) #__ queries up to the parent. Get room topic name. icontains makes sure whatever value is in the topic__name, at least contains what is in q
 
     topics = Topic.objects.all()
+    room_count = rooms.count()
 
-    context = {'rooms': rooms, 'topics': topics}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
     return render(request, 'base/home.html', context)
 
 def room(request, pk):
